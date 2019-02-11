797b4f578dc43c070a96970b769a352c3bafb100
jaEimele24 <46973541+jaEimele24@users.noreply.github.com>
2019-02-11 14:44:39 -0500

    Update multiplication-table.py

--- multiplication-table.py
+++ multiplication-table.py
@@ -1,7 +1,7 @@
 """
 multiplication-table.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: James Eiler
+Credit: Tutorials
 Assignment:
 
 Write and submit a Python program that prints a multiplication table. The user 
@@ -21,3 +21,10 @@ Height of multiplication table: 8
   7  14  21  28  35  42  49  56  63  70
   8  16  24  32  40  48  56  64  72  80
 """
+width=int(input('Width of multiplication table '))
+height=int(input('Height of multiplication table '))
+for i in range(0,width):
+    for j in range(0,height):
+        reallist=[i,j]
+        reallist2=("x" for x in reallist)
+        print(reallist2)
